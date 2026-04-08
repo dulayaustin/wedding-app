eb8c0f7f738b16610ebb003654fa2c67080b890c
Austin Dulay <dulayaustin@gmail.com>
2026-04-08 22:19:17 +0800

    Lint fix

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class Users::RegistrationsController < Devise::RegistrationsController
-  before_action :configure_sign_up_params, only: [:create]
+  before_action :configure_sign_up_params, only: [ :create ]
 
   def new
     self.resource = resource_class.new
